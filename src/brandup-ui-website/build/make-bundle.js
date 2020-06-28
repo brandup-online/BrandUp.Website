1615d3fcce851224a2ef498b76110f7dedf82bd7
Dmitry <d.kovyazin@hotmail.com>
2020-06-29 03:13:18 +0700

    Refactoring build script.

--- src/brandup-ui-website/build/make-bundle.js
+++ src/brandup-ui-website/build/make-bundle.js
@@ -1,14 +1,13 @@
 ﻿const path = require("path");
-var dtsBundle = require('dts-bundle');
+const dtsBundle = require('dts-bundle');
 const pkg = require("../package.json");
 
-const packageName = pkg.name;
 const compiledPath = path.join(__dirname, "..", "source");
 const distNpmPath = path.join(__dirname, "..", "dist");
 
 async function build() {
     dtsBundle.bundle({
-        name: packageName,
+        name: pkg.name,
         main: path.join(compiledPath, 'index.d.ts'),
         out: path.join(distNpmPath, 'index.d.ts')
     });
