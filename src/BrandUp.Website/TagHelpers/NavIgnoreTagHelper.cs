c1a3ffd7d671111c41a2882e8ed01a68c37e6d2f
Dmitry <d.kovyazin@hotmail.com>
2020-07-04 14:51:29 +0700

    Navigation tag helpers.

---
+++ src/BrandUp.Website/TagHelpers/NavIgnoreTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BrandUp.Website.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "nav-ignore")]
+    public class NavIgnoreTagHelper : TagHelper
+    {
+        const string AttributeName = "data-nav-ignore";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!output.Attributes.ContainsName(AttributeName))
+                output.Attributes.SetAttribute(AttributeName, string.Empty);
+        }
+    }
+}
---
+++ src/BrandUp.Website/TagHelpers/NavReplaceTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BrandUp.Website.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "nav-replace")]
+    public class NavReplaceTagHelper : TagHelper
+    {
+        const string ReplaceAttributeName = "data-nav-replace";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!output.Attributes.ContainsName(ReplaceAttributeName))
+                output.Attributes.SetAttribute(new TagHelperAttribute(ReplaceAttributeName, null, HtmlAttributeValueStyle.Minimized));
+
+            if (output.Attributes.TryGetAttribute("nav-replace", out TagHelperAttribute a))
+                output.Attributes.Remove(a);
+        }
+    }
+}
