68c13d1289eb95158e8a100c296f1ac4165d9afb
Dmitry <d.kovyazin@hotmail.com>
2020-07-06 21:00:04 +0700

    IQueryCollection extension method.

---
+++ src/BrandUp.Website/Extensions/IQueryCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BrandUp.Website
+{
+    public static class IQueryCollectionExtensions
+    {
+        public static bool TryGetValue(this IQueryCollection collection, string name, out string value)
+        {
+            if (!collection.TryGetValue(name, out Microsoft.Extensions.Primitives.StringValues values))
+            {
+                value = default;
+                return false;
+            }
+
+            value = values[0];
+            return true;
+        }
+    }
+}
