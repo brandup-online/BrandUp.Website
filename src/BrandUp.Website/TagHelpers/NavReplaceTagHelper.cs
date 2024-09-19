aaea500daa72a9cea1935c31e87907481aab18c7
Dmitry Kovyazin <it@brandup.online>
2024-09-20 02:19:55 +0700

    update packages

--- src/BrandUp.Website/TagHelpers/NavReplaceTagHelper.cs
+++ src/BrandUp.Website/TagHelpers/NavReplaceTagHelper.cs
@@ -5,15 +5,22 @@ namespace BrandUp.Website.TagHelpers
     [HtmlTargetElement(Attributes = "nav-replace")]
     public class NavReplaceTagHelper : TagHelper
     {
-        const string AttributeName = "data-nav-replace";
+        const string ReplaceAttributeName = "data-nav-replace";
+        const string ScopeAttributeName = "data-nav-scope";
 
         [HtmlAttributeName("nav-replace")]
         public bool NavReplace { get; set; }
 
+        [HtmlAttributeName("nav-scope")]
+        public string NavScope { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (NavReplace)
-                output.Attributes.SetAttribute(new TagHelperAttribute(AttributeName, null, HtmlAttributeValueStyle.Minimized));
+                output.Attributes.SetAttribute(new TagHelperAttribute(ReplaceAttributeName, null, HtmlAttributeValueStyle.Minimized));
+
+            if (NavScope != null)
+                output.Attributes.SetAttribute(new TagHelperAttribute(ScopeAttributeName, NavScope));
         }
     }
 }
